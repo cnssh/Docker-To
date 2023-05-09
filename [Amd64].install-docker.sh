7ee1fc27aebf58fbab53823abefb6077b0348b74
cnyea <132898007+cnyea@users.noreply.github.com>
2023-05-09 18:48:29 +0800

    Update [Amd64].install-docker.sh

--- [Amd64].install-docker.sh
+++ [Amd64].install-docker.sh
@@ -1,1 +1,17 @@
-
+echo  _____   __   _   _____   _____   _____        _____   __   _   _____   _____       ___   _       _            _____   _____   _____   _   _    _____   _____   
+echo /  ___| |  \ | | |_   _| /  _  \ /  ___/      |_   _| |  \ | | /  ___/ |_   _|     /   | | |     | |          |  _  \ /  _  \ /  ___| | | / /  | ____| |  _  \  
+echo | |     |   \| |   | |   | | | | | |___         | |   |   \| | | |___    | |      / /| | | |     | |          | | | | | | | | | |     | |/ /   | |__   | |_| |  
+echo | |     | |\   |   | |   | | | | \___  \        | |   | |\   | \___  \   | |     / / | | | |     | |          | | | | | | | | | |     | |\ \   |  __|  |  _  /  
+echo | |___  | | \  |   | |   | |_| |  ___| |        | |   | | \  |  ___| |   | |    / /  | | | |___  | |___       | |_| | | |_| | | |___  | | \ \  | |___  | | \ \  
+echo \_____| |_|  \_|   |_|   \_____/ /_____/        |_|   |_|  \_| /_____/   |_|   /_/   |_| |_____| |_____|      |_____/ \_____/ \_____| |_|  \_\ |_____| |_|  \_\  
+echo UOS install Docker 
+echo Version 1.0.0
+echo Repo:https://github.com/cntos/Docker-To/
+echo Obtain sudo mode
+su
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo apt-get install ca-certificates
+sudo apt-get install curl
+sudo apt-get install gnupg2
+sudo apt-get install software-properties-common
